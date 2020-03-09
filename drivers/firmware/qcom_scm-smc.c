2e6ba9a6eeb526eb0710b9b12ad6229d6e174d1e
Shawn Guo <shawn.guo@linaro.org>
2020-03-09 20:52:18 +0800

    firmware: qcom_scm: Add qcom_scm_set_cold_boot_addr_mc() API
    It adds qcom_scm_mc_boot_available() and qcom_scm_set_cold_boot_addr_mc()
    API to support secondary cores booting on MSM8939, where PSCI support
    is not available.
    
    The API is a shortcut call from SMP code where qcom_scm driver hasn't
    probed yet.  Therefore device pointer is not available in this case, and
    we can not call into DMA map API, but have to flush cache manually.
    
    Signed-off-by: Shawn Guo <shawn.guo@linaro.org>

--- drivers/firmware/qcom_scm-smc.c
+++ drivers/firmware/qcom_scm-smc.c
@@ -12,6 +12,8 @@
 #include <linux/arm-smccc.h>
 #include <linux/dma-mapping.h>
 
+#include <asm/cacheflush.h>
+
 #include "qcom_scm.h"
 
 /**
@@ -124,12 +126,18 @@ int __scm_smc_call(struct device *dev, const struct qcom_scm_desc *desc,
 						      SCM_SMC_FIRST_EXT_IDX]);
 		}
 
-		args_phys = dma_map_single(dev, args_virt, alloc_len,
-					   DMA_TO_DEVICE);
+		if (dev) {
+			args_phys = dma_map_single(dev, args_virt, alloc_len,
+						   DMA_TO_DEVICE);
 
-		if (dma_mapping_error(dev, args_phys)) {
-			kfree(args_virt);
-			return -ENOMEM;
+			if (dma_mapping_error(dev, args_phys)) {
+				kfree(args_virt);
+				return -ENOMEM;
+			}
+		} else {
+			args_phys = virt_to_phys(args_virt);
+			dcache_clean_inval_poc((unsigned long)args_virt,
+					       (unsigned long)args_virt + alloc_len);
 		}
 
 		smc.args[SCM_SMC_LAST_REG_IDX] = args_phys;
@@ -138,7 +146,9 @@ int __scm_smc_call(struct device *dev, const struct qcom_scm_desc *desc,
 	__scm_smc_do(&smc, &smc_res, atomic);
 
 	if (args_virt) {
-		dma_unmap_single(dev, args_phys, alloc_len, DMA_TO_DEVICE);
+		if (dev)
+			dma_unmap_single(dev, args_phys, alloc_len,
+					 DMA_TO_DEVICE);
 		kfree(args_virt);
 	}
 
