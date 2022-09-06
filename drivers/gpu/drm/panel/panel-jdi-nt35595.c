b2d4b408f40f79b90e623609872779ba61466aa1
Ketan Mate <kpmate94@gmail.com>
2022-09-06 06:05:39 +0530

    ferrari: Fix MIPI_DSI_MODE_EOT_PACKET flag name
    Rename MIPI_DSI_MODE_EOT_PACKET to MIPI_DSI_MODE_NO_EOT_PACKET because the name is opposite to its actual effect.
    Link: https://github.com/torvalds/linux/commit/0f3b68b66a6deb41c3c0eb805bb3f407083d2f57
    
    Signed-off-by: Ketan Mate <kpmate94@gmail.com>

--- drivers/gpu/drm/panel/panel-jdi-nt35595.c
+++ drivers/gpu/drm/panel/panel-jdi-nt35595.c
@@ -218,7 +218,7 @@ static int jdi_nt35595_probe(struct mipi_dsi_device *dsi)
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
-			  MIPI_DSI_MODE_EOT_PACKET |
+			  MIPI_DSI_MODE_NO_EOT_PACKET |
 			  MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM;
 
 	drm_panel_init(&ctx->panel, dev, &jdi_nt35595_panel_funcs,
--- drivers/gpu/drm/panel/panel-sharp-rsp61322.c
+++ drivers/gpu/drm/panel/panel-sharp-rsp61322.c
@@ -233,7 +233,7 @@ static int sharp_rsp61322_probe(struct mipi_dsi_device *dsi)
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
-			  MIPI_DSI_MODE_EOT_PACKET |
+			  MIPI_DSI_MODE_NO_EOT_PACKET |
 			  MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM;
 
 	drm_panel_init(&ctx->panel, dev, &sharp_rsp61322_panel_funcs,
